2fb49dc2c83a1aa60cc487a2d5b6e5c94056e3ee
Jonathan Mesa <mesa@Jonathans-MacBook-Pro.local>
2023-05-16 14:02:04 +0800

    finished tutorial

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
 function App() {
+const [todo, setTodo] = useState("");
+const [todoList, setTodoList] = useState([]);
+
   return (
     <div className="App">
-      starting fresh
+      <Header></Header>
+      <Form 
+        todo={todo} 
+        setTodo={setTodo} 
+        todoList={todoList} 
+        setTodoList={setTodoList}
+      ></Form>
+      <TodoList todoList={todoList} setTodoList={setTodoList}/>
     </div>
   );
 }
